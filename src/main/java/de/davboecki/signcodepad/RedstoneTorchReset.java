14493c06f95b04a5e963c5af8b3ec353d77935be
Oliwer <oe222ez@student.lnu.se>
2022-02-08 02:42:11 +0100

    Bugfix reset codepad after 'OK' code

--- src/main/java/de/davboecki/signcodepad/RedstoneTorchReset.java
+++ src/main/java/de/davboecki/signcodepad/RedstoneTorchReset.java
@@ -4,16 +4,23 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class RedstoneTorchReset extends SignReseter{
 	Block block;
-	public RedstoneTorchReset(Block pblock,long after,Sign psign,Player player){
+	PlayerInteractEvent event;
+	SignCodePad plugin;
+	public RedstoneTorchReset(SignCodePad p, PlayerInteractEvent e, Block pblock,long after,Sign psign,Player player){
 		super(after,psign,player);
 		block = pblock;
+		event = e;
+		plugin = p;
+
 	}
 
 	public void reset(){
-		super.reset();
+		super.reset(plugin, event);
 		if(block.getType() == Material.REDSTONE_WALL_TORCH){
 			BlockChangerTask.Blocks.add(block);
 			SignCodePad.getInstance().getServer().getScheduler().callSyncMethod(SignCodePad.getInstance(), new BlockChangerTask());
