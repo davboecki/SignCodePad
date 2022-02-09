d4f1ef6500b8ecbd32ea95211bf38367874d0538
Oliwer <oe222ez@student.lnu.se>
2022-02-09 20:50:20 +0100

    Fix color CalNoteThread

--- src/main/java/de/davboecki/signcodepad/CalNoteThread.java
+++ src/main/java/de/davboecki/signcodepad/CalNoteThread.java
@@ -2,6 +2,8 @@ package de.davboecki.signcodepad;
 
 import org.bukkit.entity.Player;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class CalNoteThread extends Reseter{
 
 	public CalNoteThread(Player pPlayer){
@@ -9,13 +11,13 @@ public class CalNoteThread extends Reseter{
 	}
 
 	public void reset(){
-		player.sendMessage("§cYou need to Calibrate the SignCodePad to your TexturePack.");
-		player.sendMessage("§cPlease create a sign with this:");
-		player.sendMessage("§d          [SignCodePad]");
-		player.sendMessage("§d                Cal");
+		player.sendMessage(ChatColor.RED + "You need to Calibrate the SignCodePad to your TexturePack.");
+		player.sendMessage(ChatColor.RED + "Please create a sign with this:");
+		player.sendMessage(ChatColor.LIGHT_PURPLE + "          [SignCodePad]");
+		player.sendMessage(ChatColor.LIGHT_PURPLE + "                Cal");
 		player.sendMessage("");
 		player.sendMessage("");
-		player.sendMessage("§cto calibrate the plugin.");
-		player.sendMessage("§cIf you change your TexturePack please do this once more.");
+		player.sendMessage(ChatColor.RED + "to calibrate the plugin.");
+		player.sendMessage(ChatColor.RED + "If you change your TexturePack please do this once more.");
 	}
 }
